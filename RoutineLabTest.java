45e78f20fe650169b9c3373b6320ab46631503e4
Nivethitha-07 <srimuthunivethitha@gmail.com>
2025-10-15 23:56:47 +0530

    Add RoutineLabTest class for routine lab tests

---
+++ RoutineLabTest.java
@@ -0,0 +1,9 @@
+package hospital.lab.management;
+        public class RoutineLabTest extends LabTest {
+
+public RoutineLabTest(String testName, double price) {
+super(testName, price);
+}public String getType() {
+return "Routine Test";
+}
+}
